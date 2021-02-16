1333ce895b306c42c3ad1815452e60a6925fa3f2
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-02-15 22:11:19 -0200

    Criacao do projeto e implementacao do microservico da loja

---
+++ loja/src/main/java/com/devmos/microservice/loja/controller/CompraController.java
@@ -0,0 +1,20 @@
+package com.devmos.microservice.loja.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devmos.microservice.loja.controller.dto.CompraDTO;
+
+@RestController
+@RequestMapping("/compra")
+public class CompraController {
+	
+	@PostMapping
+	public void realizaCompra(@RequestBody CompraDTO compra){
+		//TODO - criar logica da realizacao da compra
+		
+	}
+
+}
