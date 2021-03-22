2d87df29fae70f455878f0eaa146ea1449904964
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-03-21 21:24:30 -0300

    Adicionando log na busca de um produto

--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/controller/ProdutoController.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/controller/ProdutoController.java
@@ -2,6 +2,8 @@ package br.com.alura.microservice.fornecedor.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +19,12 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoService produtoService;
 	
+	private static final Logger logger = LoggerFactory.getLogger(ProdutoController.class);
+
+	
 	@RequestMapping("/{estado}")
 	public List<Produto> getProdutosPorEstado(@PathVariable("estado") String estado) {
+		logger.info("Buscando um produo pelo estado " + estado);
 		return produtoService.getProdutosPorEstado(estado);
 	}
 }
